d173df8234f352ff073fb6ef786fda7fdc6bda41
zumrydeen <zumrydeen.prs@gmail.com>
2019-07-04 15:00:23 +0530

    DB Creation Live

--- config/database.php
+++ config/database.php
@@ -63,6 +63,19 @@ return [
             ]) : [],
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'db_laravel_test'),
+            'username'  => env('DB_TEST_USERNAME', 'root'),
+            'password'  => env('DB_TEST_PASSWORD', 'root'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
